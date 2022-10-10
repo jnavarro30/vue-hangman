1b539e55ff2df87503b4ea8f3271fcb133e69683
Jesse Navarro <navarro@macbook.local>
2022-10-09 23:57:15 -0700

    setup vue and add app template

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MainGame msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import MainGame from './components/MainGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainGame
+  }
+}
+</script>
+
+<style>
+:root {
+  --main-text-color: orange;
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url("./assets/stick__figure.jpeg");
+}
+</style>
---
+++ src/components/MainGame.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main_game">
+    <h1 class="main_title">Hangman</h1>
+    <div class="main_canvas"></div>
+    <div class="main_word">- - - - - - - -</div>
+    <div class="main_letters"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main_game {
+  border: 8px solid var(--main-text-color);
+  border-radius: 10px;
+  height: 80%;
+  width: 80%;
+  background-color: black;
+  text-align: center;
+}
+
+.main_title {
+  color: var(--main-text-color);
+}
+
+.main_canvas {
+  border: 2px solid var(--main-text-color);
+  height: 200px;
+  width: 400px;
+  margin: 0 auto;
+}
+
+.main_word {
+  color: white;
+  font-size: 3rem;
+  padding: 1rem;
+}
+
+.main_letters  {
+  border: 2px solid var(--main-text-color);
+  height: 300px;
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 5rem;
+}
+</style>
