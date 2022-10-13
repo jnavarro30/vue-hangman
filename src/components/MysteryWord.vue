f85d0c4b334b357c1848050b588927b1476fba74
Jesse Navarro <navarro@macbook.local>
2022-10-13 01:34:14 -0700

    add api and get a random word

--- src/components/MysteryWord.vue
+++ src/components/MysteryWord.vue
@@ -1,10 +1,31 @@
 <template>
-    <div class="mystery-word">- - - - - - - -</div>
+    <button @click="getWord()">New Word</button>
+    <div class="mystery-word">{{ mysteryWord }}</div>
 </template>
 
 <script>
 export default {
-    name: 'MysteryWord'
+    name: 'MysteryWord',
+    data() {
+      return {
+        mysteryWord: '- - - - - - - -',
+        testWords: ['one', 'two', 'three', 'four']
+      }
+    },
+    
+    methods: {
+        async getWord() {
+          let res = await fetch('https://random-word-api.herokuapp.com/word');
+          let data = await res.json();
+          console.log(this.mysteryWord);
+          this.mysteryWord = data[0];
+          console.log(data);
+        }
+      ,
+      hideWord() {
+        
+      }
+    },
 }
 </script>
 
@@ -12,6 +33,12 @@ export default {
 .mystery-word {
   color: white;
   font-size: 3rem;
-  padding: 1rem;
+  
+}
+
+button {
+  margin: 2rem;
+  padding: .5rem;
+  cursor: pointer;
 }
 </style>
