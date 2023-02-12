9d470c6efb55be25d7054b32f18b707aca1c0579
jnavarro30 <jnavarro30@live.com>
2023-02-12 13:29:52 -0800

    main game in progress

--- tests/unit/MainGame.spec.js
+++ tests/unit/MainGame.spec.js
@@ -1,6 +1,9 @@
-import { shallowMount } from '@vue/test-utils';
-import MainGame from '../../src/components/MainGame';
-import CanvasScreen from '../../src/components/CanvasScreen';
+import { mount } from '@vue/test-utils';
+import MainGame from '@/components/MainGame';
+import HeaderTitle from '@/components/HeaderTitle';
+import ChosenWord from '@/components/ChosenWord';
+import AlphabetKeyboard from '@/components/AlphabetKeyboard';
+import CanvasScreen from '@/components/CanvasScreen';
 import 'jest-canvas-mock';
 require('whatwg-fetch')
 
@@ -11,12 +14,35 @@ require('whatwg-fetch')
 // )
 
 describe('MainGame.vue', () => {
-    it ('button click should fetch word', async () => {
-        // shallowMount(CanvasScreen);
-        // const wrapper = shallowMount(MainGame);
-        // await wrapper.vm.newWord();
-        // let fetchedWord = await wrapper.vm.chosenWord;
-        // expect(fetch).toHaveBeenCalledTimes(1);
-        // expect(fetchedWord).toEqual('hello');
+    const wrapper = mount(MainGame);
+
+    it('should render all child components', () => {
+        const  headerTitle = wrapper.findComponent(HeaderTitle);
+        const  chosenWord = wrapper.findComponent(ChosenWord);
+        const  alphabetKeyboard = wrapper.findComponent(AlphabetKeyboard);
+        const  canvasScreen = wrapper.findComponent(CanvasScreen);
+        const components =  [headerTitle, chosenWord, alphabetKeyboard, canvasScreen];
+
+        components.forEach(component => {
+            expect(component.exists()).toBe(true);
+        })
     })
+
+    it('should trigger newWord function', async () => {
+        let buttonOne = wrapper.findAll('button')[0];
+        await buttonOne.trigger('click');
+        expect(wrapper.vm.newWord()).toHaveBeenCalled(1);
+     })
+
+
+
+    // it('button click should fetch word', async () => {
+    //     // shallowMount(CanvasScreen);
+    //     // const wrapper = shallowMount(MainGame);
+    //     // await wrapper.vm.newWord();
+    //     // let fetchedWord = await wrapper.vm.chosenWord;
+    //     // expect(fetch).toHaveBeenCalledTimes(1);
+    //     // expect(fetchedWord).toEqual('hello');
+    //     console.log(wrapper.vm,  '@@@@@@@@@')
+    // })
 })
